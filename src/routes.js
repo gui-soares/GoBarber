7128cccd36aeb08898165b12304bc614c6383a54
gui-soares <eng.guilhermemsoares@gmail.com>
2019-05-02 22:25:41 -0300

    Autenticaçao de usuario

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,15 @@ const upload = require('multer')(multerConfig)
 const routes = express.Router()
 
 const UserController = require('./app/controller/userController')
+const SessionController = require('./app/controller/sessionController')
+
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
 
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/dashboard', (req, res) => {
+  return res.render('dashboard')
+})
 module.exports = routes
