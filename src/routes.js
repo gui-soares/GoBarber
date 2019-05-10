0f7b647a68b635f8fc39e6be328751ce591c40b3
gui-soares <eng.guilhermemsoares@gmail.com>
2019-05-10 00:31:46 -0300

    Agendamento de serviço

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ const SessionController = require('./app/controller/sessionController')
 const DashboardController = require('./app/controller/dashboardController')
 const FileController = require('./app/controller/fileController')
 const AppointmentController = require('./app/controller/appointmentController')
+const AvailableController = require('./app/controller/availableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -35,5 +36,7 @@ routes.get('/app/logout', SessionController.destroy)
 routes.get('/app/dashboard', DashboardController.index)
 
 routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.post('/app/appointments/new/:provider', AppointmentController.store)
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
